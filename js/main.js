499208ce91cc909c3346555a654a9cd08c5de92f
julian deca <juliandecastelli@gmail.com>
2024-10-29 08:47:09 -0300

    add eventListeners

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
 const keys = [[
     "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"], // Top row
     ["A", "S", "D", "F", "G", "H", "J", "K", "L"],       // Middle row
-    ["Z", "X", "C", "V", "B", "N", "M"]                  // Bottom row
+    ["Enter","Z", "X", "C", "V", "B", "N", "M","delete"]                  // Bottom row
   ];
 class Square {
     constructor(id, letter){
         this.id = id;
         this.letter = letter;
-        this.html = `<div id="${this.id}" class="square">${this.letter}</div>`;
+        this.setHtml();
+        this.isEmpty = true;
     }
     updateHtml(letter){
         this.letter = letter;
         this.html = `<div id="${this.id}" class="square">${this.letter}</div>`
     }
-    
+    setHtml(){
+        this.html = `<div id="${this.id}" class="square">${this.letter}</div>`;
+    }
+    setLetter(letter){
+        this.letter = letter;
+        this.isEmpty = false;
+        this.setHtml();
+    }
 }
 class Line {
     constructor(id){
@@ -46,13 +54,25 @@ class Keyboard {
         this.baseHtml = `<div class="keyboard">`
         this.createKeys();
         this.mapKeys();
+        this.htmlKeys = document.getElementsByClassName("key")
+
     }
     createKeys(){
         this.html = this.baseHtml;
         this.keys.forEach(line => {
             this.html += `<div class="k-row">`;
             line.forEach(key => {
-                this.html += `<div id="${key}" class="key">${key}</div>`
+                if (key=="Enter"){
+                    this.html += `<div id="${key}" class="btn btn-secondary button key">${key}</div>`
+
+                }
+                else if (key=="delete"){
+                    this.html += `<div id="${key}" class="button "> <i id="${key}" class="fi fi-tr-delete delete"></i></div>`
+                }
+                else{
+
+                    this.html += `<div id="${key}" class="key">${key}</div>`
+                }
             });
             this.html += `</div>`;
         })
@@ -75,8 +95,17 @@ class Game {
         this.keyboard = new Keyboard(keys);
         this.mapLines();
         addEventListener('keyup',(e)=>{
-            console.log(e.key)        
+            this.addLetter(e.key);
         });
+        Array.from(this.keyboard.htmlKeys).forEach((key)=>{
+            key.addEventListener("click", ()=>{
+                this.addLetter(key.textContent);
+            });
+            key.addEventListener("touchstart", ()=>{
+                this.addLetter(key.textContent);
+            });
+        });
+        
     }
     createLines(){
         let lines = [];
@@ -90,9 +119,22 @@ class Game {
         const container = document.getElementById('container');
         container.innerHTML = "";
         this.lines.forEach((line)=>{
-                console.log(line.html);
                 container.innerHTML += line.html;;        
         })
+    }
+    addLetter(letter) {
+        letter = letter.toUpperCase();
+        const emptyLine  = this.lines.find((line)=>!line.isPlayed)
+        if(emptyLine){
+            const emptySquare = emptyLine.squares.find((square)=>square.isEmpty);
+            if(emptySquare){
+                if(letter.length == 1 && letter != " ")
+                emptySquare.setLetter(letter);
+                emptyLine.mapSquares();
+                this.mapLines();
+                // console.log(emptyLine);
+            }
+        }
     }
 }
 const game = new Game();
